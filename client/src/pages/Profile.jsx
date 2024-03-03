72b8646f80b9640b1dcd9882ef3c843b694e112e
Monae Curbeam <curbeammonae@gmail.com>
2024-03-03 08:37:53 -0500

    image functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,52 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useRef } from 'react'
+import { useState, useEffect } from 'react'
+import { getStorage, uploadBytes, uploadBytesResumable} from 'firebase/storage'
+import { app }  from '../firebase'
 
 export default function Profile() {
+  const fileRef = useRef(null)
   const {currentUser} = useSelector((state) => state.user)
+  const [file, setFile] = useState(undefined)
+  console.log(file)
+
+  // firebase storage
+  // allow read;
+  // allow write: if 
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
+  useEffect(() => {
+    if(file){
+      handleFileUpload();
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed', 
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred /
+      snapshot.totalBytes) * 100;
+      console.log('upload is' + progress + '% done')
+    },
+    )
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center'>
         Profile
       </h1>
       <form className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
+        {/* allows us to upload only images */}
+        <input onChange={(e)=>setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image/*'/>
+        
+        <img onClick={()=> fileRef.current.click()}src={currentUser.avatar} className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
         <input type='text' id='username'placeholder='username'className=' p-3 rounded-lg'/>
         <input type='email' id='email'placeholder='email'className=' p-3 rounded-lg'/>
         <input type='password' id='password'placeholder='password'className=' p-3 rounded-lg'/>
